0a8d08581da34782df30c7c16657fe643e01a18c
NATHANIM-REPO <natimc225@gmail.com>
2024-09-09 00:49:23 +0300

    finished styling the button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ window.addEventListener("scroll", function() {
     const header = document.getElementById("header2");
     const text = document.getElementById('headertext2');
     const button = document.getElementById('reserve')
+    const icon = document.getElementById('dropdown')
 
     if (window.scrollY > 50) {
       header.style.backgroundColor = "#ffffff";  
@@ -9,12 +10,16 @@ window.addEventListener("scroll", function() {
       button.style.backgroundColor = 'black'
       button.style.color = 'white'
       button.style.alignItems = 'center'
+      button.style.justifyContent = "center"
+      icon.style.display = "none"
       
     } else {
       header.style.backgroundColor = "#514848";  
       text.style.color = "white"
       button.style.backgroundColor = "white"
       button.style.color = "black"  
-        button.style.alignItems = 'center'
+      button.style.alignItems = 'center'
+      button.style.justifyContent = "center"
+      icon.style.display = 'inline'
     }
   });

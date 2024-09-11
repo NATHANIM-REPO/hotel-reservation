26f40faddc5ca83ec77e11de68e220879e5d7d9b
NATHANIM-REPO <natimc225@gmail.com>
2024-09-11 16:46:24 +0300

    added the side bar

--- index.js
+++ index.js
@@ -74,3 +74,11 @@ function plusSlides(n){
   }
 }
 
+function toggleSidebar() {
+  var sidebar = document.getElementById('sidebar');
+  if (sidebar.style.right === '0px') {
+    sidebar.style.right = '-250px'; 
+  } else {
+    sidebar.style.right = '0px'; 
+  }
+}
